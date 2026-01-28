2930b7e7152100ff1cffa17b8d20e74feb2f4674
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-01-28 19:32:22 +0530

    fixed some bugs in some url's

--- PersonalPort.js
+++ PersonalPort.js
@@ -1,23 +1,23 @@
 
-var typed = new Typed (".typing",{
-    strings:["Web Designer","Developer","programer","fast learner"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+var typed = new Typed(".typing", {
+  strings: ["Web Designer", "Developer", "programer", "fast learner"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
 })
 
 
 
-  const backToTop = document.getElementById("backToTop");
+const backToTop = document.getElementById("backToTop");
 
-  window.addEventListener("scroll", () => {
-    backToTop.style.display = window.scrollY > 300 ? "block" : "none";
-  });
+window.addEventListener("scroll", () => {
+  backToTop.style.display = window.scrollY > 300 ? "block" : "none";
+});
 
-  backToTop.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
   });
+});
 
